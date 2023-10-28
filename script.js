6634146f403398ec81d95a72480cef45f0bdbc96
irais-valenzuela <iraisvq8@gmail.com>
2023-10-28 16:43:36 -0700

    added sorting functionality

--- script.js
+++ script.js
@@ -25,11 +25,13 @@ const genZTerms = [
   { genZTerm: "Slay", meaning: "Basically you're killing it. " },
   {
     genZTerm: "FOMO",
-    meaning: "Fear of Missing Out, the feeling of missing out on something exciting or interesting.",
+    meaning:
+      "Fear of Missing Out, the feeling of missing out on something exciting or interesting.",
   },
   {
     genZTerm: "Lit",
-    meaning: "Describing something that's exciting, amazing, or highly enjoyable.",
+    meaning:
+      "Describing something that's exciting, amazing, or highly enjoyable.",
   },
   {
     genZTerm: "Flex",
@@ -37,40 +39,49 @@ const genZTerms = [
   },
   {
     genZTerm: "Clout",
-    meaning: "Influence, especially on social media, or reputation and popularity.",
+    meaning:
+      "Influence, especially on social media, or reputation and popularity.",
   },
   {
     genZTerm: "Yeet",
-    meaning: "An exclamation of enthusiasm, triumph, or excitement, often used when throwing or discarding something."
-  }, 
+    meaning:
+      "An exclamation of enthusiasm, triumph, or excitement, often used when throwing or discarding something.",
+  },
   {
     genZTerm: "Spill the Tea",
-    meaning: "To share the latest gossip or information, often used in a gossipy context.",
+    meaning:
+      "To share the latest gossip or information, often used in a gossipy context.",
   },
   {
     genZTerm: "Glow Up",
-    meaning: "A transformation or improvement in someone's appearance or life over time.",
+    meaning:
+      "A transformation or improvement in someone's appearance or life over time.",
   },
   {
     genZTerm: "Vibe Check",
-    meaning: "Assessing someone's or something's current mood or overall feeling.",
+    meaning:
+      "Assessing someone's or something's current mood or overall feeling.",
   },
   {
     genZTerm: "Ghosted",
-    meaning: "When someone suddenly stops responding to messages and disappears from communication without explanation.",
+    meaning:
+      "When someone suddenly stops responding to messages and disappears from communication without explanation.",
   },
   {
     genZTerm: "Simp",
-    meaning: "A term used to describe someone, usually a man, who is overly attentive, submissive, or affectionate towards someone they are attracted to."
-  }, 
+    meaning:
+      "A term used to describe someone, usually a man, who is overly attentive, submissive, or affectionate towards someone they are attracted to.",
+  },
   {
     genZTerm: "Sus",
-    meaning: "Short for 'suspicious' or 'suspect,' often used to describe something or someone as shady or untrustworthy."
+    meaning:
+      "Short for 'suspicious' or 'suspect,' often used to describe something or someone as shady or untrustworthy.",
   },
   {
     genZTerm: "G.O.A.T.",
-    meaning: "An acronym for 'Greatest of All Time,' used to praise someone as the best in their field or at something."
-  }
+    meaning:
+      "An acronym for 'Greatest of All Time,' used to praise someone as the best in their field or at something.",
+  },
 ];
 
 const generateCards = (termArray) => {
@@ -86,4 +97,33 @@ const generateCards = (termArray) => {
   return divs;
 };
 
+// called when cards were sorted alphabetially and need to update main tag
+const mainTag = document.querySelector("main");
+const buttonTag = document.querySelector("button");
+
+//getting main tag and assigning the html to be the return of generate cards
 document.querySelector("main").innerHTML = `${generateCards(genZTerms)}`;
+
+
+const updateUI = (cards) => {
+  mainTag.innerHTML = `${generateCards(cards)}`;
+  buttonTag.innerHTML =
+    buttonTag.innerHTML === "Sort A-Z" ? "Undo Sort" : "Sort A-Z";
+};
+
+// called upon clicking sort A-Z
+const sortCardsClick = () => {
+  if (buttonTag.innerHTML === "Undo Sort") {
+    updateUI(genZTerms);
+  } else {
+    const copy = [...genZTerms]
+    const newSortedArr = copy.sort((a, b) => {
+      return a.genZTerm.localeCompare(b.genZTerm);
+    });
+    updateUI(newSortedArr);
+  }
+};
+
+// click event
+const sortButton = document.getElementById("sort-button");
+sortButton.addEventListener("click", sortCardsClick);
